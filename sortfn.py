9a8cfcc58e7cc8f134d7c5a86a099b0ec0365bb2
shun <shun@example.com>
2016-05-05 11:24:58 +0800

    add three files

---
+++ sortfn.py
@@ -0,0 +1,24 @@
+def mySort(cmp, *args):
+    ret = []
+    for arg in args:
+        for idx, value in enumerate(ret):
+            if cmp(arg, value):
+                ret.insert(idx, arg)
+                break
+        else:
+            ret.append(arg)
+    return ret
+
+
+def cmp1(x, y):
+    return x >= y
+
+
+def cmp2(x, y):
+    return x <= y
+
+
+print("Using cmp1")
+print(mySort(cmp1, 1, 3, 7, 2, 9, 4, 6, 8))
+print("Using cmp2")
+print(mySort(cmp2, 1, 3, 7, 2, 9, 4, 6, 8))
---
+++ test.py
@@ -0,0 +1,8 @@
+def testfun(num1, num2):
+    if num1 >= num2:
+        print("{0} is bigger then {1}".format(num1, num2))
+
+
+def test2(n1, n2):
+    if n1 >= n2:
+        print("{0} is bigger then {1}".format(n1, n2))
