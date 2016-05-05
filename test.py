0895c76b08f642167ed43fc7033853f241e539ac
shun <shun@example.com>
2016-05-05 15:51:05 +0800

    add two lines

--- test.py
+++ test.py
@@ -4,6 +4,9 @@ def testfun(num1, num2):
         print("{0} is bigger then {1}".format(num1, num2))
 
 
+# Add a new line too.
 def test2(n1, n2):
     if n1 >= n2:
         print("{0} is bigger then {1}".format(n1, n2))
+
+# Add a new line
